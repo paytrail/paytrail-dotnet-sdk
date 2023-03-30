a0a479f95a16330bcc6c8ee1b844775f70f511ff
htnhu <devs+paytrail@nerdcloud.co>
2023-03-30 16:37:15 +0700

    PP6-15: Init unit test for create refund request

---
+++ Paytrail-dotnet-sdk.UnitTest/PayTrailClient-RefundPaymentTests.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using Paytrail_dotnet_sdk.Model.Request;
+using Paytrail_dotnet_sdk.Model.Request.RequestModels;
+using Paytrail_dotnet_sdk.Model.Response;
+
+namespace Paytrail_dotnet_sdk.UnitTest
+{
+    public class PayTrailClient_RefundPaymentTests
+    {
+        const string MERCHANTIDN = "375917";
+        const string SECRETKEYN = "SAIPPUAKAUPPIAS";
+        const string MERCHANTIDSIS = "695861";
+        const string SECRETKEYSIS = "MONISAIPPUAKAUPPIAS";
+
+        [Fact]
+        public void RefundPayment_RequestNull_ReturnCode200()
+        {
+            //Arrange
+            int expected = (int)Paytrail_dotnet_sdk.Util.ErrorMessage.RequestNull;
+
+            //Act
+            PayTrailClient payTrail = new PayTrailClient(MERCHANTIDSIS, SECRETKEYSIS, "test");
+            string? transactionId = null;
+            RefundRequest? refundRequest = null;
+            RefundResponse res = payTrail.RefundPayment(refundRequest, transactionId);
+            int actual = res.ReturnCode;
+            string a = res.ReturnMessage;
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void RefundPayment_Success_ReturnCode1()
+        {
+            //Arrage
+            int expected = (int)Paytrail_dotnet_sdk.Util.ErrorMessage.Success;
+
+            //Act
+            PayTrailClient ptrail = new PayTrailClient(MERCHANTIDN, SECRETKEYN, "test");
+            string transactionId = "4bd78702-c9ea-11ed-beb7-ab93e0fdf0aa";
+            List<RefundItem> it = new List<RefundItem>();
+            string refundStamp = Guid.NewGuid().ToString();
+            RefundRequest refundRequest = new RefundRequest()
+            {
+                amount = 1590,
+                email = "test@gmail.com",
+                refundStamp = refundStamp,
+                refundReference = "1234",
+                callbackUrls = new Model.Request.RequestModels.CallbackUrl
+                {
+                    cancel = "https://ecom.example.org/refund/cancel",
+                    success = "https://ecom.example.org/refund/success"
+                },
+                items = new RefundItem[0],
+            };
+            RefundResponse res = ptrail.RefundPayment(refundRequest, transactionId);
+            int actual = res.ReturnCode;
+            string a = res.ReturnMessage;
+            string data = JsonConvert.SerializeObject(res.data);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void RefundPayment_CallPayTrailReturnNull_ReturnCode300()
+        {
+            //Arrage
+            int expected = (int)Paytrail_dotnet_sdk.Util.ErrorMessage.ResponseNull;
+
+            //Act
+            PayTrailClient payTrail = new PayTrailClient(MERCHANTIDSIS, SECRETKEYSIS, "test");
+            string transactionId = "2f8a77ce-c861-11ed-be51-07513ab7f2f0";
+            RefundRequest refundRequest = new RefundRequest(){};
+
+            RefundResponse res = payTrail.RefundPayment(refundRequest, transactionId);
+
+            int actual = res.ReturnCode;
+            string a = res.ReturnMessage;
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+
+        [Fact]
+        public void RefundPayment_CallPayException_ReturnCode100()
+        {
+            //Arrage
+            int expected = (int)Paytrail_dotnet_sdk.Util.ErrorMessage.Exception;
+
+            //Act
+            PayTrailClient payTrail = new PayTrailClient(MERCHANTIDSIS, SECRETKEYSIS, "test");
+            string transactionId = "2f8a77ce-c861-11ed-be51-07513ab7f2f0";
+            RefundRequest refundRequest = new RefundRequest()
+            {
+                items = new RefundItem[0],
+            };
+
+            RefundResponse res = payTrail.RefundPayment(refundRequest, transactionId);
+            int actual = res.ReturnCode;
+            string a = res.ReturnMessage;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
