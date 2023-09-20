340f8dded9adddaa4fd38f648aa8b9cfbb36752c
NerdCloud <devs+paytrail@nerdcloud.co>
2023-09-20 14:26:39 +0700

    fix/add-class-response

---
+++ Paytrail-dotnet-sdk/Model/Response/RevertAuthorizationHoldResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Paytrail_dotnet_sdk.Model.Response
+{
+    public class RevertAuthorizationHoldResponse : Response
+    {
+        public RevertAuthorizationHoldData Data { get; set; }
+    }
+
+    public class RevertAuthorizationHoldData
+    {
+        public string TransactionId { get; set; }
+    }
+}
